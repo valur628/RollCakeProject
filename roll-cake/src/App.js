32c6e18c44d8dfd7b530f4590a4e0362edc235fc
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-07-24 16:06:39 +0900

    2021-07-24

--- roll-cake/src/App.js
+++ roll-cake/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
+  // let text = "text 1";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button onClick={onSubmit}>로그인</button>
+      </form>
     </div>
   );
 }
