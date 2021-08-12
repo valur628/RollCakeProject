adac3d21bcd59f3635ea8cb638eb943f52b2ee43
wncjf2000 <unconfigured@null.spigotmc.org>
2021-08-12 15:45:51 +0900

    234234

--- roll-cake/src/pages/MainPage.js
+++ roll-cake/src/pages/MainPage.js
@@ -10,29 +10,35 @@ import { SlideCardItem } from "../components/Compo_cards";
 function MainPage() {
   return (
     <>
-  <Carousel>
-    <Carousel.Item>
-      <CarouselDiv>
-      <CarouselImage
-        className="d-block w-100"
-        src="https://github.com/wncjf2000/RollCakeProject/blob/main/roll-cake/src/image/carousel_1.png?raw=true"
-        alt="First slide"
-      />
-      <CarouselSlideCardItem><SlideCardItem
-        DevName="CD 프로젝트 레드"
-        SoftName="사이버펑크 2077"
-        AfterCost="50,000"
-        DisRate="25">
-      </SlideCardItem></CarouselSlideCardItem>
-      </CarouselDiv>
-    </Carousel.Item>
-    {/*<Carousel.Item>
+      <Carousel>
+        <Carousel.Item>
+          <CarouselDiv>
+            <CarouselImage
+              className="d-block w-100"
+              src="https://github.com/wncjf2000/RollCakeProject/blob/main/roll-cake/src/image/carousel_1.png?raw=true"
+              alt="First slide"
+            />
+            <CarouselLogoImage
+              src="https://github.com/wncjf2000/RollCakeProject/blob/main/roll-cake/src/image/carousel_logo_1.png?raw=true" alt="Logo">
+            </CarouselLogoImage>
+            <CarouselSlideCardItem><SlideCardItem
+              DevName="CD 프로젝트 레드"
+              SoftName="사이버펑크 2077"
+              AfterCost="50,000"
+              DisRate="25">
+            </SlideCardItem></CarouselSlideCardItem>
+          </CarouselDiv>
+        </Carousel.Item>
+        <Carousel.Item>
       <CarouselDiv>
       <CarouselImage
         className="d-block w-100"
         src="https://github.com/wncjf2000/RollCakeProject/blob/main/roll-cake/src/image/carousel_2.png?raw=true"
         alt="Second slide"
       />
+      <CarouselLogoImageInvert
+        src="https://github.com/wncjf2000/RollCakeProject/blob/main/roll-cake/src/image/carousel_logo_2.png?raw=true" alt="Logo">
+      </CarouselLogoImageInvert>
       <CarouselSlideCardItem><SlideCardItem
         DevName="EA DICE"
         SoftName="배틀필드 2042"
@@ -48,6 +54,9 @@ function MainPage() {
         src="https://github.com/wncjf2000/RollCakeProject/blob/main/roll-cake/src/image/carousel_3.png?raw=true"
         alt="Third slide"
       />
+      <CarouselLogoImage
+        src="https://github.com/wncjf2000/RollCakeProject/blob/main/roll-cake/src/image/carousel_logo_3.png?raw=true" alt="Logo">
+      </CarouselLogoImage>
       <CarouselSlideCardItem><SlideCardItem
         DevName="락스타 게임즈"
         SoftName="레드 데드 리뎀션"
@@ -55,9 +64,9 @@ function MainPage() {
         DisRate="100">
       </SlideCardItem></CarouselSlideCardItem>
       </CarouselDiv>
-    </Carousel.Item>*/}
-  </Carousel>
-  </>
+    </Carousel.Item>
+      </Carousel>
+    </>
   );
 }
 const CarouselDiv = styled.div`
@@ -75,11 +84,24 @@ width: auto;
 height: 100%;
 object-fit: cover;
 `;
+const CarouselLogoImage = styled.img`
+width: 25%;
+position: absolute;
+top: 35%;
+left: 18%;
+`;
+const CarouselLogoImageInvert = styled.img`
+width: 25%;
+position: absolute;
+top: 35%;
+left: 18%;
+filter: invert(100%);
+`;
 const CarouselSlideCardItem = styled.div`
 width: 100%;
 height: 100%;
 position: absolute;
-top: 30%;
+top: 25%;
 left: 58%;
 `;
 
