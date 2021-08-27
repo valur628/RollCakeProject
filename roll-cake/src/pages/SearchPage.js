ac63616634aae5f9cf7416426c3c754a2594dc26
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-08-27 15:33:14 +0900

    2021-08-27

--- roll-cake/src/pages/SearchPage.js
+++ roll-cake/src/pages/SearchPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import tempData from "../TempData.json";
 
-import { LargeCardItem } from "../components/Compo_cards";
+import { LargeCardItem, MediumCardItem } from "../components/Compo_cards";
 
 const filterDeals = (data, searchDeals) => {
   if (!searchDeals) {
@@ -18,7 +18,7 @@ const filterDeals = (data, searchDeals) => {
 };
 
 const SearchPage = () => {
-  const [fillterdDeal, setData] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
     //   axios
     //     .get(
@@ -35,24 +35,27 @@ const SearchPage = () => {
   const { search } = window.location;
   const query = new URLSearchParams(search).get("s");
   const [searchQuery, setSearchQuery] = useState(query || "");
-  const filteredDeals = filterDeals(fillterdDeal, searchQuery);
+  const filteredDeals = filterDeals(data, searchQuery);
   console.log(filteredDeals);
 
   const LargeCardMapTable = styled.table`
     margin-left: auto;
     margin-right: auto;
+    vertical-align: middle;
   `;
-  const LargeCardHeaderTd = styled.td`
+  const SearchToolbar = styled.div`
     margin-left: auto;
     margin-right: auto;
-    padding-top: 2.5%;
+    width: 100%;
+    height: 100%;
+    max-width: 1200px;
+    max-height: 170px;
     vertical-align: middle;
     text-align: center;
-    font-size: 100%;
-    padding-bottom: 2.5%;
   `;
   const LargeCardMapDiv = styled.div`
-    padding-bottom: 4%;
+    padding-top: 2%;
+    padding-bottom: 2%;
   `;
 
   return (
@@ -61,52 +64,49 @@ const SearchPage = () => {
       {/* <Hotdeals hotdeals={filteredDeals} /> */}
       <div>
         <LargeCardMapTable>
-          <LargeCardMapDiv />
-          <h2>'{searchQuery}' 검색결과</h2>
+          <LargeCardMapDiv>
+            <h2>'{searchQuery}' 검색결과</h2>
+          </LargeCardMapDiv>
           <tr>
-            <LargeCardHeaderTd>
-              <div
-                className="btn-group btn-group-lg btn-group-custom"
-                role="group"
-                aria-label="toolbar"
-              >
-                <button type="button" className="btn btn-dark">
-                  전체 ({filteredDeals.length})
-                </button>
-                <button type="button" className="btn btn-dark">
-                  게임 ({filteredDeals.length})
-                </button>
-                <button type="button" className="btn btn-dark">
-                  소프트웨어 ({filteredDeals.length})
-                </button>
-                <button type="button" className="btn btn-dark">
-                  개발사 ({filteredDeals.length})
-                </button>
-                <button type="button" className="btn btn-dark">
-                  태그 ({filteredDeals.length})
-                </button>
-              </div>
-            </LargeCardHeaderTd>
+            <SearchToolbar
+              className="btn-group btn-group-lg btn-group-custom"
+              role="group"
+              aria-label="toolbar"
+            >
+              <button type="button" className="btn btn-dark">
+                전체 ({filteredDeals.length})
+              </button>
+              <button type="button" className="btn btn-dark">
+                게임 ({filteredDeals.length})
+              </button>
+              <button type="button" className="btn btn-dark">
+                소프트웨어 ({filteredDeals.length})
+              </button>
+              <button type="button" className="btn btn-dark">
+                개발사 ({filteredDeals.length})
+              </button>
+              <button type="button" className="btn btn-dark">
+                태그 ({filteredDeals.length})
+              </button>
+            </SearchToolbar>
           </tr>
           <tr>
-            <td>
-              {filteredDeals.map((deal) => {
-                return (
-                  <LargeCardMapDiv>
-                    <LargeCardItem
-                      key={deal.id}
-                      Picture={deal.Picture}
-                      DevName={deal.DevName}
-                      SoftName={deal.SoftName}
-                      BeforeCost={deal.BeforeCost}
-                      AfterCost={deal.AfterCost}
-                      DisRate={deal.DisRate}
-                      Platform={deal.Platform}
-                    ></LargeCardItem>
-                  </LargeCardMapDiv>
-                );
-              })}
-            </td>
+            {filteredDeals.map((deal) => {
+              return (
+                <LargeCardMapDiv>
+                  <LargeCardItem
+                    key={deal.id}
+                    Picture={deal.Picture}
+                    DevName={deal.DevName}
+                    SoftName={deal.SoftName}
+                    BeforeCost={deal.BeforeCost}
+                    AfterCost={deal.AfterCost}
+                    DisRate={deal.DisRate}
+                    Platform={deal.Platform}
+                  />
+                </LargeCardMapDiv>
+              );
+            })}
           </tr>
         </LargeCardMapTable>
       </div>
