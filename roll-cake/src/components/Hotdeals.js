cef0ad7aebd2fa4f2103f36b897e6bac2229c28d
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-10-01 21:15:48 +0900

    실패....

--- roll-cake/src/components/Hotdeals.js
+++ roll-cake/src/components/Hotdeals.js
@@ -13,7 +13,7 @@ function compareNumbers(a, b) {
   return a - b;
 }
 
-const Hotdeals = ({ hotdeals, order = "latest" }) => {
+const Hotdeals = ({ hotdeals, order = "na" }) => {
   // order 파라미터를 만듦
   // HotdealPage, SearchPage에서 파라미터를 받아 카드 정렬 방법 변경
   // 최신순: latest, 인기순: popularity , 이름순: name , 가격순: price, 할인율 순: disRate
@@ -23,19 +23,22 @@ const Hotdeals = ({ hotdeals, order = "latest" }) => {
     case "latest":
       // id로 정렬 id는 데이터가 서버에 저장된 시간
       console.log("order: ", order);
-      // hotdeals.sort(function (a, b) {
-      //   var nameA = a.id.toUpperCase(); // ignore upper and lowercase
-      //   var nameB = b.id.toUpperCase(); // ignore upper and lowercase
-      //   if (nameA < nameB) {
-      //     return -1;
-      //   }
-      //   if (nameA > nameB) {
-      //     return 1;
-      //   }
+      hotdeals.ScrapingDB.sort(
+        () =>
+          function (a, b) {
+            var nameA = a.id.toUpperCase(); // ignore upper and lowercase
+            var nameB = b.id.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
 
-      //   // 이름이 같을 경우
-      //   return 0;
-      // });
+            // 이름이 같을 경우
+            return 0;
+          }
+      );
       break;
     case "popularity":
       console.log("order: ", order);
@@ -44,31 +47,40 @@ const Hotdeals = ({ hotdeals, order = "latest" }) => {
     case "name":
       // software의 이름 순으로 정렬
       console.log("order: ", order);
-      hotdeals.sort(function (a, b) {
-        var nameA = a.DB_SoftName.toUpperCase(); // ignore upper and lowercase
-        var nameB = b.DB_SoftName.toUpperCase(); // ignore upper and lowercase
-        if (nameA < nameB) {
-          return -1;
-        }
-        if (nameA > nameB) {
-          return 1;
-        }
+      hotdeals.ScrapingDB.sort(
+        () =>
+          function (a, b) {
+            var nameA = a.SoftName.toUpperCase(); // ignore upper and lowercase
+            var nameB = b.SoftName.toUpperCase(); // ignore upper and lowercase
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
 
-        // 이름이 같을 경우
-        return 0;
-      });
+            // 이름이 같을 경우
+            return 0;
+          }
+      );
       break;
     case "price":
       console.log("order: ", order);
-      hotdeals.sort(function (a, b) {
-        return a.DB_DisCost - b.DB_DisCost;
-      });
+      hotdeals.ScrapingDB.sort(
+        () =>
+          function (a, b) {
+            return a.DB_DisCost - b.DB_DisCost;
+          }
+      );
       break;
     case "disRate":
       console.log("order: ", order);
-      hotdeals.sort(function (a, b) {
-        return a.DB_DisRate - b.DB_DisRate;
-      });
+      hotdeals.ScrapingDB.sort(
+        () =>
+          function (a, b) {
+            return a.DB_DisRate - b.DB_DisRate;
+          }
+      );
       break;
     default:
       console.log("Error: ", order);
@@ -77,7 +89,7 @@ const Hotdeals = ({ hotdeals, order = "latest" }) => {
 
   return (
     <>
-      {hotdeals.map((deal) => {
+      {hotdeals.ScrapingDB.map((deal) => {
         return (
           <LargeCardMapDiv>
             <LargeCardItem
@@ -89,6 +101,14 @@ const Hotdeals = ({ hotdeals, order = "latest" }) => {
               AfterCost={deal.DB_DisCost / 100}
               DisRate={deal.DB_DisRate / 100}
               Platform={deal.DB_Platform}
+              // key={deal.id}
+              // Picture={deal.Picture}
+              // DevName={deal.DevName}
+              // SoftName={deal.SoftName}
+              // BeforeCost={deal.BeforeCost}
+              // AfterCost={deal.AfterCost}
+              // DisRate={deal.DisRate}
+              // Platform={deal.Platform}
             />
           </LargeCardMapDiv>
         );
--- roll-cake/src/pages/HotdealPage.js
+++ roll-cake/src/pages/HotdealPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import tempData from "../DBresult.json";
+// import tempData from "../TempData.json";
 
 import Hotdeals from "../components/Hotdeals";
 
